17e5d447655ec9901b396a6347f044c12300a9a9
Rick Evans <javaswinger@yahoo.com>
2013-09-11 17:43:28 -0400

    default should be non mySQL password

--- env.rb
+++ env.rb
@@ -5,4 +5,4 @@ require dotenv if File.exist?(dotenv)
 ENV['DATABASE'] ||= 'city_of_raleigh_crime'
 ENV['DATABASE_HOST'] ||= 'localhost'
 ENV['DATABASE_USER'] ||= 'root'
-ENV['DATABASE_PASSWORD'] ||= 'root'
+ENV['DATABASE_PASSWORD'] ||= ''
